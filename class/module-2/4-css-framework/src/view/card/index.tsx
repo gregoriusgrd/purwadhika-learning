f3d415659f38bf23d5bad1c7437b6eb8eb08e32f
Gregorius G <gregorius.gerald21@gmail.com>
2025-05-16 21:43:09 +0700

    add prettier plugin

--- class/module-2/4-css-framework/src/view/card/index.tsx
+++ class/module-2/4-css-framework/src/view/card/index.tsx
@@ -1,29 +1,29 @@
 export default function CardView() {
   return (
-    <main className="flex justify-center items-center h-screen m-0 bg-[#f4d04e]">
-      <div className="w-[327px] h-[501px] lg:w-[384px] lg:h-[522px] p-[24px] border-[1px] border-[#111111] rounded-[20px] bg-white shadow-[8px_8px_0px_0px_black]">
+    <main className="m-0 flex h-screen items-center justify-center bg-[#f4d04e]">
+      <div className="h-[501px] w-[327px] rounded-[20px] border-[1px] border-[#111111] bg-white p-[24px] shadow-[8px_8px_0px_0px_black] lg:h-[522px] lg:w-[384px]">
         <div>
           <img
-            className="w-full h-full"
+            className="h-full w-full"
             src="/card/blog-image.png"
             alt="Blog image"
           />
         </div>
 
-        <div className="mt-[24px] mb-[24px] lg:text-[14px]">
-          <div className="flex justify-center items-center w-[73px] h-[26px] rounded-[4px] font-figtree font-medium text-[12px] leading-[150%] tracking-[0%] text-[#111111] bg-[#f4d04e]">
+        <div className="mt-[24px] mb-[24px] flex lg:text-[14px]">
+          <div className="font-figtree flex h-[26px] w-[73px] items-center justify-center rounded-[4px] bg-[#f4d04e] text-[12px] leading-[150%] font-medium tracking-[0%] text-[#111111]">
             <p>Learning</p>
           </div>
 
-          <div className="font-figtree font-medium text-[12px] lg:text-[14px] leading-[150%] tracking-[0%] text-[#111111]">
-            <p className="mb-3 mt-3">Published 21 Dec 2023</p>
+          <div className="font-figtree flex text-[12px] leading-[150%] font-medium tracking-[0%] text-[#111111] lg:text-[14px]">
+            <p className="mt-3 mb-3">Published 21 Dec 2023</p>
           </div>
 
-          <div className="font-figtree font-extrabold text-[20px] lg:text-2xl  leading-[150%] tracking-[0%] text-[#111111]">
+          <div className="font-figtree text-[20px] leading-[150%] font-extrabold tracking-[0%] text-[#111111] lg:text-2xl">
             <p className="mb-3">HTML & CSS foundations</p>
           </div>
 
-          <div className="font-figtree font-medium text-[14px] leading-[150%] tracking-[0%] text-[#6b6b6b]">
+          <div className="font-figtree text-[14px] leading-[150%] font-medium tracking-[0%] text-[#6b6b6b]">
             <p className="">
               These languages are the backbone of every website, defining
               structure, content, and presentation.
@@ -31,7 +31,7 @@ export default function CardView() {
           </div>
         </div>
 
-        <div className="flex justify-start items-center font-figtree font-extrabold text-[14px] leading-[150%] tracking-[0%] text-[#111111]">
+        <div className="font-figtree flex items-center justify-start text-[14px] leading-[150%] font-extrabold tracking-[0%] text-[#111111]">
           <img className="mr-3" src="/card/user-pic.png" alt="Foto user" />
           <span>Greg Hooper</span>
         </div>
