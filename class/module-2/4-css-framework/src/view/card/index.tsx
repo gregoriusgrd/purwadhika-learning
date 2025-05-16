d47d5fab9d28ed752e4f56a00900d481821d0855
Gregorius G <gregorius.gerald21@gmail.com>
2025-05-17 02:40:32 +0700

    style

--- class/module-2/4-css-framework/src/view/card/index.tsx
+++ class/module-2/4-css-framework/src/view/card/index.tsx
@@ -1,25 +1,25 @@
 export default function CardView() {
   return (
     <main className="m-0 flex h-screen items-center justify-center bg-[#f4d04e]">
-      <div className="min-h-[501px] w-[327px] overflow-hidden rounded-[20px] border-[1px] border-[#111111] bg-white p-[24px] shadow-[8px_8px_0px_0px_black] lg:min-h-0 lg:w-[384px]">
+      <div className="min-h-[501px] w-[327px] overflow-hidden rounded-[20px] border-[1px] border-[#111111] bg-white p-6 shadow-[8px_8px_0px_0px_black] lg:min-h-0 lg:w-[384px]">
         <div>
           <img className="w-full" src="/card/blog-image.png" alt="Blog image" />
         </div>
 
         <div className="mt-[24px] flex flex-col gap-3 lg:text-[14px]">
-          <div className="font-figtree flex h-[26px] w-[73px] items-center justify-center rounded-[4px] bg-[#f4d04e] text-[12px] leading-[150%] font-extrabold tracking-[0%] text-[#111111] lg:text-[14px]">
+          <div className="font-figtree flex h-[26px] w-[73px] items-center justify-center rounded-[4px] bg-[#f4d04e] text-[12px] leading-[150%] font-extrabold tracking-normal text-[#111111] lg:text-[14px]">
             Learning
           </div>
 
-          <div className="font-figtree flex text-[12px] leading-[150%] font-medium tracking-[0%] text-[#111111] lg:text-[14px]">
+          <div className="font-figtree flex text-[12px] leading-[150%] font-medium tracking-normal text-[#111111] lg:text-[14px]">
             Published 21 Dec 2023
           </div>
 
-          <div className="font-figtree text-[20px] leading-[150%] font-extrabold tracking-[0%] text-[#111111] lg:text-2xl">
+          <div className="font-figtree text-[20px] leading-[150%] font-extrabold tracking-normal text-[#111111] lg:text-2xl">
             HTML & CSS foundations
           </div>
 
-          <div className="font-figtree text-[14px] leading-[150%] font-medium tracking-[0%] text-[#6b6b6b] lg:text-[16px]">
+          <div className="font-figtree text-[14px] leading-[150%] font-medium tracking-normal text-[#6b6b6b] lg:text-[16px]">
             These languages are the backbone of every website, defining
             structure, content, and presentation.
           </div>
