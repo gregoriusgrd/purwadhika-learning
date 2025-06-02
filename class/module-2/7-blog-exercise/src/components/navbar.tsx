d4386bc37b2d2b81a9d506423238ca99888282ec
Gregorius G <gregorius.gerald21@gmail.com>
2025-06-02 22:31:42 +0700

    add react icons

--- class/module-2/7-blog-exercise/src/components/navbar.tsx
+++ class/module-2/7-blog-exercise/src/components/navbar.tsx
@@ -1,28 +1,55 @@
 "use client";
 
+import Image from 'next/image';
+import { BiSearch } from 'react-icons/bi';
+
 export default function Navbar() {
   return (
     <nav className="flex-shrink-0 flex justify-around items-center px-8 py-6 bg-[rgba(0,_0,_0,_0.5)]">
-    <div className="h-10">
-      <img className="nav__logo-img" src="hero-logo.png" alt="Trazler Logo" />
-    </div>
+      <div className="h-10">
+        <Image src="/navbar/hero-logo.png" layout="responsive" width={100} height={40} className="nav__logo-img" alt="Trazler Logo" />
+      </div>
 
-    <ul className="flex gap-8">
-      <li><a href="#" className="uppercase no-underline text-white font-medium">Destinations</a></li>
-      <li><a href="#" className="uppercase no-underline text-white font-medium">Food</a></li>
-      <li><a href="#" className="uppercase no-underline text-white font-medium">Well Being</a></li>
-      <li><a href="#" className="uppercase no-underline text-white font-medium">Sport</a></li>
-      <li><a href="#" className="uppercase no-underline text-white font-medium">Family</a></li>
-      <li><a href="#" className="uppercase no-underline text-white font-medium">LifeStyle</a></li>
-    </ul>
+      <ul className="flex gap-8">
+        <li>
+          <a href="#" className="uppercase no-underline text-white font-medium">
+            Destinations
+          </a>
+        </li>
+        <li>
+          <a href="#" className="uppercase no-underline text-white font-medium">
+            Food
+          </a>
+        </li>
+        <li>
+          <a href="#" className="uppercase no-underline text-white font-medium">
+            Well Being
+          </a>
+        </li>
+        <li>
+          <a href="#" className="uppercase no-underline text-white font-medium">
+            Sport
+          </a>
+        </li>
+        <li>
+          <a href="#" className="uppercase no-underline text-white font-medium">
+            Family
+          </a>
+        </li>
+        <li>
+          <a href="#" className="uppercase no-underline text-white font-medium">
+            LifeStyle
+          </a>
+        </li>
+      </ul>
 
-    <div className="flex items-center gap-6 text-xs max-h-14">
-      <i className="bx bx-search"></i>
-      <button className="w-80 uppercase bg-black text-white px-10 py-3.5 rounded border-none cursor-pointer">
-        Get Your $120 Christmas Gift
-      </button>
-    </div>
-  </nav>
+      <div className="flex items-center gap-6 text-xs max-h-14">
+        <BiSearch className='text-white text-xl'/>
+        <button className="w-80 uppercase bg-black text-white px-10 py-3.5 rounded border-none cursor-pointer">
+          Get Your $120 Christmas Gift
+        </button>
+      </div>
+    </nav>
   );
 }
 
