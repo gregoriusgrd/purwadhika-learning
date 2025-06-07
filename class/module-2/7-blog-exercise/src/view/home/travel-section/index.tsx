f3c799ed82dd02d7db78ef674ae80d5f7c6bd6a6
Gregorius G <gregorius.gerald21@gmail.com>
2025-06-08 02:17:52 +0700

    style travel section

--- class/module-2/7-blog-exercise/src/view/home/travel-section/index.tsx
+++ class/module-2/7-blog-exercise/src/view/home/travel-section/index.tsx
@@ -4,19 +4,22 @@ export default function HeroSection() {
   return (
     <section className="bg-white px-36 py-24">
       <div className="h-[600px] bg-[url('/home/travel-section/travel-background.jpg')] bg-cover bg-center rounded">
-        <div className="flex h-full flex-col items-center justify-center text-center gap-5">
+      <div className="h-full bg-[rgba(0,_0,_0,_0.5)]">
+
+        <div className="px-36 flex h-full flex-col items-center justify-center text-center">
           <p className="bg-[rgba(255,255,255,0.4)] text-white font-bold rounded px-2 py-1 ">Travel</p>
-          <h1 className="text-4xl font-bold">
+          <h1 className="mt-2.5 text-4xl font-bold">
             Richird Norton photorealistic rendering as real photos
           </h1>
-          <p className="text-lg">
+          <p className="mt-5 text-lg font-bold">
             Progressively incentivize cooperative systems through technically
             sound functionalities. The credibly productivate seamless data.
           </p>
 
-          <button className="h-12 w-80 cursor-pointer rounded-lg border-none bg-white text-lg text-violet-500">
+          <button className="mt-7 h-12 w-80 cursor-pointer rounded-lg border-none bg-white text-lg text-violet-500">
             Start planning your trip
           </button>
+      </div>
         </div>
       </div>
     </section>
