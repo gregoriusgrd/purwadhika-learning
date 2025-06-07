716d830972bb118c9c84207f66f947ef9158c17e
Gregorius G <gregorius.gerald21@gmail.com>
2025-06-07 15:03:12 +0700

    style

--- class/module-2/7-blog-exercise/src/components/blog/blog-card.tsx
+++ class/module-2/7-blog-exercise/src/components/blog/blog-card.tsx
@@ -1,6 +1,10 @@
 import { IBlog } from "@/interface/blog.interface";
 import Image from "next/image";
 
+import { BiShareAlt } from "react-icons/bi";
+import { BiWifi0 } from "react-icons/bi";
+
+
 interface BlogCardProps {
   blog: IBlog;
 }
@@ -13,6 +17,7 @@ export default function BlogCard({ blog }: BlogCardProps) {
 
   return (
     <article>
+
       {thumbnail?.fields?.file?.url && (
         <Image
           src={`https:${thumbnail.fields.file.url}`}
@@ -22,21 +27,29 @@ export default function BlogCard({ blog }: BlogCardProps) {
           className="w-full h-60 object-cover rounded mb-4"
         />
       )}
-      <div>
+
+      <div className="flex flex-col gap-7">
         <h2 className="text-4xl font-bold text-gray-900">{title}</h2>
 
-        <div>
+        <div className="flex items-center justify-around">
           {authorImage?.fields?.file?.url && (
             <Image
               src={`https:${authorImage.fields.file.url}`}
               alt={`Foto ${authorName}`}
               width={40}
               height={40}
-              className="object-cover rounded mb-4"
+              className="object-cover rounded"
             />
           )}
           <p className="text-2xl text-gray-900">{authorName}</p>
+          <hr className="border-t border-gray-600 w-8 border-0 opacity-40" />
+
           <p className="text-lg text-gray-600">{publishDate}</p>
+
+          <BiShareAlt className="text-gray-600" />
+          <p className="text-lg text-gray-600">1K</p>
+          <p className="text-lg text-gray-600">shares</p>
+          
         </div>
 
         <p className="text-gray-600 text-xl">{description}</p>
