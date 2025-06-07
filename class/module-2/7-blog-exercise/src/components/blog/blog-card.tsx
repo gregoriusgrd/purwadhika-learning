6ce11c900f5adf3f1f7b611829e08a4d29afabcf
Gregorius G <gregorius.gerald21@gmail.com>
2025-06-07 15:43:53 +0700

    card edit

--- class/module-2/7-blog-exercise/src/components/blog/blog-card.tsx
+++ class/module-2/7-blog-exercise/src/components/blog/blog-card.tsx
@@ -4,55 +4,62 @@ import Image from "next/image";
 import { BiShareAlt } from "react-icons/bi";
 import { BiWifi0 } from "react-icons/bi";
 
-
 interface BlogCardProps {
   blog: IBlog;
 }
 
 export default function BlogCard({ blog }: BlogCardProps) {
-  const { title, authorName, publishDate, description, thumbnail, authorImage } =
-    blog.fields;
+  const {
+    title,
+    authorName,
+    publishDate,
+    description,
+    thumbnail,
+    authorImage,
+  } = blog.fields;
 
   console.log("thumbnail:", blog.fields.thumbnail);
 
   return (
     <article>
-
       {thumbnail?.fields?.file?.url && (
         <Image
           src={`https:${thumbnail.fields.file.url}`}
           alt={`Thumbnail ${title}`}
           width={400}
           height={250}
-          className="w-full h-60 object-cover rounded mb-4"
+          className="mb-4 h-60 w-full rounded object-cover"
         />
       )}
 
       <div className="flex flex-col gap-7">
         <h2 className="text-4xl font-bold text-gray-900">{title}</h2>
 
-        <div className="flex items-center justify-around">
-          {authorImage?.fields?.file?.url && (
-            <Image
-              src={`https:${authorImage.fields.file.url}`}
-              alt={`Foto ${authorName}`}
-              width={40}
-              height={40}
-              className="object-cover rounded"
-            />
-          )}
-          <p className="text-2xl text-gray-900">{authorName}</p>
-          <hr className="border-t border-gray-600 w-8 border-0 opacity-40" />
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-1">
+            {authorImage?.fields?.file?.url && (
+              <Image
+                src={`https:${authorImage.fields.file.url}`}
+                alt={`Foto ${authorName}`}
+                width={40}
+                height={40}
+                className="rounded object-cover"
+              />
+            )}
+            <p className="text-base md:text-base lg:text-lg text-gray-900 font-bold">{authorName}</p>
+          </div>
 
-          <p className="text-lg text-gray-600">{publishDate}</p>
+          <hr className="w-8 border-0 border-t border-gray-600 opacity-40" />
 
-          <BiShareAlt className="text-gray-600" />
-          <p className="text-lg text-gray-600">1K</p>
-          <p className="text-lg text-gray-600">shares</p>
-          
+          <p className="text-sm md:text-base lg:text-base text-gray-600">{publishDate}</p>
+
+          <div className="flex items-center gap-1">
+            <BiShareAlt className="text-gray-600" />
+            <p className="text-base text-gray-600">1K</p>
+            <p className="text-base text-gray-600">shares</p>
+          </div>
         </div>
-
-        <p className="text-gray-600 text-xl">{description}</p>
+        <p className="text-xl text-gray-600">{description}</p>
       </div>
     </article>
   );
